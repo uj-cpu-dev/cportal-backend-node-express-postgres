a05ad2862b91176b9e95f363b27f41ce7cc61792
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-06-30 15:29:39 +0100

    testing db connection on cluster

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
 
-const POSTGRES_URL = "postgres://postgres:Winner12@@localhost:5433/customers";
+const POSTGRES_URL = "postgres://postgres:password@postgres-service:5432/postgres";
 
 const sequelize = new Sequelize(POSTGRES_URL, {
     dialect: 'postgres' // Specify the dialect as 'postgres'
